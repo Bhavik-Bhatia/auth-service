b1385b1243f5e138f1ef4187976c3966a53c8d65
bhavik bhatia <bbhatia@asite.com>
2025-01-15 13:51:06 +0530

    Revert "Auth Microservice"
    This reverts commit 01903694f3b619a1b86ba9ccd2df5a5d7cca8da6.

--- auth-core/src/main/java/com/ab/auth/exception/UserNotExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.ab.auth.exception;
-
-
-public class UserNotExistsException extends RuntimeException{
-    public UserNotExistsException(String exceptionMessage){
-        super(exceptionMessage);
-    }
-}
